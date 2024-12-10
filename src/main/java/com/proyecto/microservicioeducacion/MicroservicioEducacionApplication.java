9c390400dae5c3e30f4cad5cd9fb49b4a2f82fee
Samuu10 <svallmun@myuax.com>
2024-12-10 18:26:33 +0100

    Initial commit

---
+++ src/main/java/com/proyecto/microservicioeducacion/MicroservicioEducacionApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.microservicioeducacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioEducacionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicioEducacionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proyecto/microservicioeducacion/MicroservicioEducacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyecto.microservicioeducacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioEducacionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
