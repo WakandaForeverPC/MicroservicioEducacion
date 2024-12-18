96b6c6d11926283cda774c734627cbbf4b84ace6
Samuu10 <svallmun@myuax.com>
2024-12-18 18:49:10 +0100

    Configuración necesaria para monitoreo con Prometheus y Grafana

--- src/main/java/com/proyecto/microservicioeducacion/service/AspectoService.java
+++ src/main/java/com/proyecto/microservicioeducacion/service/AspectoService.java
@@ -10,7 +10,7 @@ public class AspectoService {
 
     public List<AspectoEducativo> getCursosInformaticos() {
         return Arrays.asList(
-                new AspectoEducativo("Cursos:", "Programación en Java, Desarrollo Web, Bases de Datos, Sping Boot, Ciberseguridad, Inteligencia Artificial, Machine Learning, Desarrollo de Apps, Cloud Computing, "),
+                new AspectoEducativo("Cursos:", "Programación en Java, Desarrollo Web, Bases de Datos, Sping Boot, Ciberseguridad, Inteligencia Artificial, Machine Learning, Desarrollo de Apps, Cloud Computing, Big Data"),
                 new AspectoEducativo("Centros:", "Universidad de Wakanda, Univeridad Politécnica de Wakanda, Universidad Mercante, Universidad Jabari, Universidad de Black Panther, Universidad de la Tierra, Universidad del Fuego, Universidad del Aire, Universidad del Agua, Universidad Minera"),
                 new AspectoEducativo("Grados Univeritarios:", "Ingeniería Informática, Ingeniería de Software, Ingeniería de Telecomunicaciones, Ingeniería Electrónica, Ingeniería Mecánica, Ingeniería Civil, Ingeniería Química, Ingeniería Biomédica, Ingeniería Aeroespacial, Ingeniería Ambiental")
         );
